bd568c87c9c48852772b76e07a8182682968ca13
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-03-29 18:10:17 +0100

    [#116528969] Build the header for Navigation Drawer and place the hamburger icon

--- app/src/main/java/com/andela/motustracker/MainActivity.java
+++ app/src/main/java/com/andela/motustracker/MainActivity.java
@@ -1,18 +1,36 @@
 package com.andela.motustracker;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         toolbar = (Toolbar) findViewById(R.id.id_toolBar);
         setSupportActionBar(toolbar);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        //synchronize it here
+        actionBarDrawerToggle.syncState();
+    }
+
 }
