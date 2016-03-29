71accc2a7df2ec9656cbb15f6005703cc149c9ad
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-03-29 22:26:01 +0100

    [#116528969] Implement onclick on the navigation icon and toolbar item

--- app/src/main/java/com/andela/motustracker/MainActivity.java
+++ app/src/main/java/com/andela/motustracker/MainActivity.java
@@ -1,15 +1,21 @@
 package com.andela.motustracker;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
                 R.string.drawer_open, R.string.drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.id_settings_drawer :
+                        //call the settings App here...
+                        drawerLayout.closeDrawers();
+                    break;
+                }
+                return true;
+            }
+        });
+
     }
 
     @Override
@@ -33,4 +54,22 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.id_settings_actionBar:
+                //call settings..
+                break;
+        }
+
+        return true;
+    }
 }
