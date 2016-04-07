113cf07b84089b98ee25fba26977a438e8add6ab
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-04-07 19:21:01 +0100

    [#116531227] Implement the Onclick on the spinner view placed above the ListView

--- app/src/main/java/com/andela/motustracker/LocationListFragment.java
+++ app/src/main/java/com/andela/motustracker/LocationListFragment.java
@@ -6,11 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 import com.andela.motustracker.dbParser.DbCursorAdapter;
+import com.andela.motustracker.helper.AppContext;
 import com.andela.motustracker.manager.DbManager;
 import com.andela.motustracker.model.LocationData;
 
@@ -21,6 +27,8 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class LocationListFragment extends Fragment {
+    private Spinner spinner;
+    private ListView listView;
 
 
     public LocationListFragment() {
@@ -34,7 +42,36 @@ public class LocationListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_location_list, container, false);
 
-        ListView listView = (ListView) view.findViewById(R.id.id_frag_listView);
+        spinner = (Spinner) view.findViewById(R.id.id_spinnerView);
+        SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(AppContext.get(),
+                R.array.spinnerDropDown, R.layout.spinner_custom_view);
+        spinner.setAdapter(spinnerAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                //final String ad = getResources().getString(R.string.list_by_name);
+                switch (item) {
+                    case "List By Day":
+                        //List location by day in the ListView
+                        break;
+                    case "List By Time Spent":
+                        //List location by time spent in the ListView
+                        break;
+                }
+
+                // Showing selected spinner item
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        listView = (ListView) view.findViewById(R.id.id_frag_listView);
         DbCursorAdapter adapter = new DbCursorAdapter(new DbManager().readDataFromDb(),0);
         listView.setAdapter(adapter);
         return view;
