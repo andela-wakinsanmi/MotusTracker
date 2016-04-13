0ae4618fdf9073ef769bbe42ad58eb525d5b2520
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-04-13 23:01:00 +0100

    Implement parcelable in LocationData.java class

--- app/src/main/java/com/andela/motustracker/model/LocationData.java
+++ app/src/main/java/com/andela/motustracker/model/LocationData.java
@@ -1,13 +1,16 @@
 package com.andela.motustracker.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Spykins on 06/04/16.
  */
-public class LocationData {
+public class LocationData implements Parcelable {
     private String address;
     private double longitude;
     private double latitude;
-    private String  date;
+    private String date;
     private Double timeSpent;
 
     public LocationData(String address, String date, double latitude, double longitude, Long timeSpent) {
@@ -16,8 +19,26 @@ public class LocationData {
         this.latitude = latitude;
         this.longitude = longitude;
         this.timeSpent = timeSpent.doubleValue();
-
     }
+
+    protected LocationData(Parcel in) {
+        address = in.readString();
+        longitude = in.readDouble();
+        latitude = in.readDouble();
+        date = in.readString();
+    }
+
+    public static final Creator<LocationData> CREATOR = new Creator<LocationData>() {
+        @Override
+        public LocationData createFromParcel(Parcel in) {
+            return new LocationData(in);
+        }
+
+        @Override
+        public LocationData[] newArray(int size) {
+            return new LocationData[size];
+        }
+    };
 
     public String getAddress() {
         return address;
@@ -28,12 +49,7 @@ public class LocationData {
     }
 
     public String getDate() {
-
         return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
     }
 
     public double getLatitude() {
@@ -60,7 +76,16 @@ public class LocationData {
         this.timeSpent = timeSpent.doubleValue();
     }
 
-    public void upDateTimeSpent(double newTime) {
-        this.timeSpent += newTime;
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(address);
+        dest.writeDouble(longitude);
+        dest.writeDouble(latitude);
+        dest.writeString(date);
     }
 }
